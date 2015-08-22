ef53a2337506e0e3f1212150b6dc71d0bb54ed71
saambha <Anoop@Anoop.local>
2015-08-22 17:19:10 +0400

    Initial Commit

---
+++ DubaiConnect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DubaiConnect
+//
+//  Created by Anoop on 8/22/15.
+//  Copyright (c) 2015 Anoop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DubaiConnect/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DubaiConnect
+//
+//  Created by Anoop on 8/22/15.
+//  Copyright (c) 2015 Anoop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
